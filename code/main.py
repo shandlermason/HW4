9ecfd1e222d13b7787ad0a964ff4d13c42dae326
shandlermason <samason1@aggies.ncat.edu>
2022-09-02 12:08:21 -0400

    Basics of Num class
    basics of Num class and basic div, mid function

--- code/main.py
+++ code/main.py
@@ -1,2 +1,14 @@
 def func(x):
     return x + 1
+
+
+class Num:
+
+    def __init__(self, x):
+        self.x = x
+
+    def div(self):
+        return self.x  # standard deviation - sort numbers, find 90th, 10th percentile, return (90th-10th)/2.56
+
+    def mid(self):
+        return self.x  # median - sort numbers seen so far, return the middle value
