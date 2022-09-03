b5cbf7b2cf3b1a5a436b8e89d0ac468ed7ccddcc
rohitgeddam <rohitgeddam2018@gmail.com>
2022-09-03 14:49:23 -0400

    implemented Nums class constructor and nums method

--- code/main.py
+++ code/main.py
@@ -4,8 +4,23 @@ def func(x):
 
 class Num:
 
-    def __init__(self, x):
-        self.x = x
+    def __init__(self, c=0, s=""):
+        self.n = 0
+        self.at = c
+        self.name = s
+        self._has = []
+        self.lo = float('inf')
+        self.hi = float('-inf')
+        self.isSorted = True
+        
+        # self.w
+        
+    def nums(self):
+        """Return kept numbers sorted"""
+        if not self.isSorted:
+            self._has.sort()
+            self.isSorted=True
+        return self._has
 
     def div(self):
         return self.x  # standard deviation - sort numbers, find 90th, 10th percentile, return (90th-10th)/2.56
