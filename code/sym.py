e3c52d85ba44346b59eb6fbffad86a3b7b11d0b8
Arun Kumar <rarunkumar15112k@gmail.com>
2022-09-05 18:42:05 -0400

    Update sym.py

--- code/sym.py
+++ code/sym.py
@@ -1,17 +1,17 @@
+class Sym:
+    def mid(self,col,most,mode):
+        most=-1
+        for k,v in self._has.items():
+            if v > most:
+                mode=k
+                most=v
+        return mode
 
-def mid(self,col,most,mode):
-    most=-1
-    for k,v in self._has.items():
-        if v > most:
-            mode=k
-            most=v
-    return mode
-
-def div(self,e,fun):
-    def fun(p):
-        return p*math.log(p,2)
-    e=0
-    for _,n in self._has.items():
-        if n>0:
-            e=e-fun(n/self.n)
-    return e
+    def div(self,e,fun):
+        def fun(p):
+            return p*math.log(p,2)
+        e=0
+        for _,n in self._has.items():
+            if n>0:
+                e=e-fun(n/self.n)
+        return e
