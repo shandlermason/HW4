f61107a5c070535bf5d9e499fe3495601e6b14ce
Darkviper7 <m.s.k.tejavarma@gmail.com>
2022-09-05 22:22:10 -0400

    Update add function

--- code/main.py
+++ code/main.py
@@ -1,22 +1,8 @@
 from distutils import ccompiler
-
+import math
 
 def func(x):
     return x + 1
-
-
-class Sym:
-    def __init__(self, c=0, s=""):
-        self.n = 0
-        self.at = c
-        self.name = s
-        self._has = []
-
-    def add(v, self=None):
-        if (v != "?"):
-            self.n = self.n + 1
-            self._has[v] = 1 + (self._has[v])  # v or 0
-
 
 class Num:
 
@@ -38,8 +24,18 @@ class Num:
             self.isSorted = True
         return self._has
 
-    def add(self):
-        return 0
+    def add(v, pos):
+        if v!="?":
+            self.n = self.n + 1
+            self.lo = math.min(v, self.lo)
+            self.hi = math.max(v, self.hi)
+            if len(self._has) < the.nums:
+                pos = 1 + len(self._has)
+            elif math.random() < the.nums/self.n:
+                pos = math.random(len(self._has))
+            if pos:
+                self.isSorted = False
+                self._has[pos] = int(v)
 
     # standard deviation
     def div(self):
