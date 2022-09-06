d68ad51fc55234239f211a6a5d5b42fa60ac1ce6
Arun Kumar <rarunkumar15112k@gmail.com>
2022-09-06 09:42:11 -0400

    Temporay update test.py

--- tests/test.py
+++ tests/test.py
@@ -3,17 +3,17 @@ from code.main import Num
 from code.sym import Sym
 import unittest
 
-class TestCases(unittest.TestCase):
-    def test_num(self):
-        num = Num(1, "name")
-        mid = num.mid()
-        div = num.div()
-        print(mid, div)
-    def test_sym():
-        sym=Sym()
-        for x in ["a","a","a","a","b","b","c"]:
-            sym.add(x)
-        mode=sym.mid()
-        entropy=sym.div()
-        print(mode,entropy) #oo has to be implemented later
-        assert mode='a' and (entropy>=1.37 and entropy<=1.38)
+# class TestCases(unittest.TestCase):
+#     def test_num(self):
+#         num = Num(1, "name")
+#         mid = num.mid()
+#         div = num.div()
+#         print(mid, div)
+def test_sym():
+    sym=Sym()
+    for x in ["a","a","a","a","b","b","c"]:
+        sym.add(x)
+    mode=sym.mid()
+    entropy=sym.div()
+    print(mode,entropy) #oo has to be implemented later
+    assert mode='a' and (entropy>=1.37 and entropy<=1.38)
