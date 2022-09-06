aa65c196ce45a2b648e99e624acb32be908035ff
shandlermason <samason1@aggies.ncat.edu>
2022-09-06 16:06:32 -0400

    updated Num test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,8 +1,7 @@
 # from code import main
-# from code.main import Num
+from code.main import Num
 # import unittest
 from code.sym import Sym
-
 
 # class TestCases(unittest.TestCase):
 #     def test_num(self):
@@ -14,11 +13,20 @@ from code.sym import Sym
 import math
 
 
+def test_num():
+    num = Num()
+    for i in range(0, 100):
+        num.add(i)
+    mid, div = num.mid(), num.div()
+    print(mid, div)
+    assert (50 <= mid <= 52) and (30.5 < div < 32)
+
+
 def test_sym():
-    sym=Sym()
-    for x in ["a","a","a","a","b","b","c"]:
+    sym = Sym()
+    for x in ["a", "a", "a", "a", "b", "b", "c"]:
         sym.add(x)
-    mode=sym.mid()
-    entropy=sym.div()
-    #print(mode,entropy) #oo has to be implemented later
-    assert mode=='a' and (entropy>=1.37 and entropy<=1.38)
+    mode = sym.mid()
+    entropy = sym.div()
+    # print(mode,entropy) #oo has to be implemented later
+    assert mode == 'a' and (1.37 <= entropy <= 1.38)
