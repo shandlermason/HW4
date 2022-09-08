1a7aa9f304c90cbb985831b2b0698c44b8809ab4
shandlermason <samason1@aggies.ncat.edu>
2022-09-08 14:24:23 -0400

    beginning of Data class

--- code/data.py
+++ code/data.py
@@ -2,4 +2,9 @@ class Data:
     def __init__(self, src):
         self.cols = None
         self.rows = {}
-        
+
+    def add(self, xs, row):
+        return 0
+
+    def stats(self, places, showCols, fun, t, v):
+        return t
