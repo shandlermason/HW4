2c04ab053494e7e006818c18b324ad77bf635d75
rohitgeddam <rohitgeddam2018@gmail.com>
2022-09-12 10:14:00 -0400

    add placeholder fn in utils.py

---
+++ code/utils.py
@@ -0,0 +1,40 @@
+# Handle settings
+def coerce():
+    pass
+
+
+def cli():
+    pass
+
+
+# Lists 
+def copy():
+    pass
+
+def per():
+    pass
+
+def push():
+    pass
+
+def csv():
+    pass
+
+
+# Strings
+def o():
+    pass
+
+def oo():
+    pass
+
+
+# Miscs
+
+def rouges():
+    pass
+
+
+# Maths
+def rnd():
+    pass
